19ede7a95aa2382cd8f23cd30f9b9deee1f8bc88
moeabdol <mohd.a.saed@gmail.com>
2017-08-26 20:47:02 +0300

    Implement basic demo-form-sku.component

---
+++ src/app/demo-form-sku/demo-form-sku.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector: "app-demo-form-sku",
+  templateUrl: "./demo-form-sku.component.html",
+  styleUrls: ["./demo-form-sku.component.scss"]
+})
+export class DemoFormSkuComponent implements OnInit {
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  onSubmit(form: any): void {
+    console.log("You submitted value: ", form);
+  }
+}
